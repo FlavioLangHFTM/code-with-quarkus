72fae65a854560607d4bec90ec086d752845c077
Flavio Lang <flavio.lang@hftm.ch>
2024-05-17 14:11:13 +0200

    Added Blog Endpoint

--- src/main/java/ch/hftm/BlogResource.java
+++ src/main/java/ch/hftm/BlogResource.java
@@ -1,16 +1,16 @@
-package org.acme;
+package ch.hftm;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/blog")
+public class BlogResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String blog() {
+        return "Willkommen auf meinem Blog!";
     }
 }
